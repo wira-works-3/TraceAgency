e19233e3f4684f577d326e0069dce681d145b4c5
Aditya Wirayudha <83379121+Adityawir0410@users.noreply.github.com>
2026-03-25 11:44:34 +0700

    fix error

--- components/SmoothScroll.jsx
+++ components/SmoothScroll.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect } from "react";
-import Lenis from "@studio-freight/lenis";
+import Lenis from "lenis";
 import { usePathname } from "next/navigation";
 
 export default function SmoothScroll({ children }) {
