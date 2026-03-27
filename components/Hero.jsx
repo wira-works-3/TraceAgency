25cf8d51f94e89b1361c23702e923c88990143bc
Aditya Wirayudha <83379121+Adityawir0410@users.noreply.github.com>
2026-03-27 23:34:15 +0700

    USHER

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -140,7 +140,7 @@ export default function Hero() {
           className="flex flex-col items-center"
         >
           <motion.div variants={itemVariants} className="inline-block mb-6 px-4 py-1.5 rounded-full border border-border text-text-secondary text-xs font-semibold tracking-[0.2em] uppercase">
-            Agency SPG/SPB
+            Agency SPG & USHER
           </motion.div>
           
           <motion.h1 
